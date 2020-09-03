b0acd830d434e16fc73a1689ee90381e723368ab
YeahnTTo <53292866+YeahnTTo@users.noreply.github.com>
2020-09-03 10:24:51 +0900

    중복 체크 추가 , 링크추가 시 https:// 정규식 추가

--- timetable.js
+++ timetable.js
@@ -40,8 +40,13 @@ $(function () {
         }
         
         // 시간 겹침 체크
-        if($(col_id).hasClass('hide') || $(col_id).attr('rowspan') > 0) {
+        console.log($(col_id).hasClass('hide'));
+        console.log( $(col_id).attr('rowspan'));
+        if($(col_id).hasClass('hide') || $(col_id).attr('rowspan') > 0 || $(col_id).text().length > 0) {
             if(confirm("시간 겹침. 그래도 입력 할테냐?")){
+                if($(col_id).text().length > 0){
+                    $(col_id).text('');
+                }
                 remove_rowspan(_day, start_t, end_t, time_gap);
                 input_col_text(col_id, name, link);
                 if(time_gap >= 2) {
@@ -61,73 +66,92 @@ $(function () {
         }
     });
 
-    // 시작 rowspan 이후로 범위 컬럼 숨기기
-    function hide_col(_day, start_t, end_t,time_gap) {
-        let tr = $("#"+_day+"-"+start_t).closest('tr');
-        let rowspan = $("#"+_day+"-"+start_t).attr('rowspan');
-        let index = $('tr').index(tr);
-        let tot = parseInt(index)+parseInt(rowspan);
-        for(var i=index+1;i<=tot;i++){
+    $("#btn-down").on('click',function(){
+        htmlDownload();
+    });
+});
+
+// 시작 rowspan 이후로 범위 컬럼 숨기기
+function hide_col(_day, start_t, end_t,time_gap) {
+    let tr = $("#"+_day+"-"+start_t).closest('tr');
+    let rowspan = $("#"+_day+"-"+start_t).attr('rowspan');
+    let index = $('tr').index(tr);
+    let tot = parseInt(index)+parseInt(rowspan);
+    for(var i=index+1;i<=tot;i++){
+        $("#"+_day+"-"+i).removeAttr('rowspan');
+        $("#"+_day+"-"+i).text('');
+        $("#"+_day+"-"+i).addClass('hide');
+    }
+
+    if($("#"+_day+"-"+end_t).attr('rowspan') !== typeof undefined && $("#"+_day+"-"+end_t).attr('rowspan')  !== false) {
+        $("#"+_day+"-"+end_t).removeAttr('rowspan');
+    }
+
+    for(let k=end_t;k<=9;k++) {
+        let attr = $("#"+_day+"-"+k).attr('rowspan');
+        // rowspan이 나올때 까지 hide클래스를 지운다.
+        if(typeof attr !== typeof undefined && attr !== false){
+            break;
+        } else {
+            $("#"+_day+"-"+k).removeClass('hide');
+        }
+    }
+}
+
+// 컬럼 위치에 과목명과 링크 삽입
+function input_col_text(col_id, name, link=null) {
+    let regx = new RegExp(/(http(s)?:\/\/)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}/gi);
+
+    if(link){
+        if(regx.test(link)){
+
+            $(col_id).append("<div onclick='window.open(\""+link+"\")'>"+name+"</div>");
+        } else {
+
+            $(col_id).append("<div onclick='window.open(\"https://"+link+"\")'>"+name+"</div>");
+        }
+    } else {
+        $(col_id).append("<div>"+name+"</div>");
+    }
+}
+
+// 덮어쓰기 할 때 겹치는 rowspan 제거
+function remove_rowspan(_day, start_t, end_t, time_gap) {
+    for(let i=start_t;i>=1;i--) {
+        let attr = $("#"+_day+"-"+i).attr('rowspan');
+        // rowspan이 있는 경우
+        if(typeof attr !== typeof undefined && attr !== false){
             $("#"+_day+"-"+i).removeAttr('rowspan');
             $("#"+_day+"-"+i).text('');
-            $("#"+_day+"-"+i).addClass('hide');
-        }
 
-        if($("#"+_day+"-"+end_t).attr('rowspan') !== typeof undefined && $("#"+_day+"-"+end_t).attr('rowspan')  !== false) {
-            $("#"+_day+"-"+end_t).removeAttr('rowspan');
-        }
-        
-        for(let k=end_t;k<=9;k++) {
-            let attr = $("#"+_day+"-"+k).attr('rowspan');
-            // rowspan이 나올때 까지 hide클래스를 지운다.
-            if(typeof attr !== typeof undefined && attr !== false){
-                break;
-            } else {
-                $("#"+_day+"-"+k).removeClass('hide');
-            }
-        }
-    }
-
-    // 컬럼 위치에 과목명과 링크 삽입
-    function input_col_text(col_id, name, link=null) {
-        $(col_id).append("<div onclick='window.open(\""+link+"\")'>"+name+"</div>");
-    }
-
-    // 덮어쓰기 할 때 겹치는 rowspan 제거
-    function remove_rowspan(_day, start_t, end_t, time_gap) {
-        for(let i=start_t;i>=1;i--) {
-            let attr = $("#"+_day+"-"+i).attr('rowspan');
-            // rowspan이 있는 경우
-            if(typeof attr !== typeof undefined && attr !== false){
-                $("#"+_day+"-"+i).removeAttr('rowspan');
-                $("#"+_day+"-"+i).text('');
-
-                if(end_t<attr) {
-                    for(let j=end_t+1;j>attr;j++) {
-                        $("#"+_day+"-"+j).removeClass('hide');
-                    }
+            if(end_t<attr) {
+                for(let j=end_t+1;j>attr;j++) {
+                    $("#"+_day+"-"+j).removeClass('hide');
                 }
-            } else {
-                $("#"+_day+"-"+i).removeClass('hide');
             }
-        }
-
-        $("#"+_day+"-"+start_t).removeClass('hide');
-        $("#"+_day+"-"+start_t).attr('rowspan', time_gap);
-
-        
-        for(let k=start_t+time_gap;k<=9;k++) {
-            let attr = $("#"+_day+"-"+k).attr('rowspan');
-            // rowspan이 나올때 까지 hide클래스를 지운다.
-            if(typeof attr !== typeof undefined && attr !== false){
-                break;
-            } else {
-                $("#"+_day+"-"+k).removeClass('hide');
-            }
+        } else {
+            $("#"+_day+"-"+i).removeClass('hide');
         }
     }
-    
-    function htmlDownload() {
-        
+
+    $("#"+_day+"-"+start_t).removeClass('hide');
+    $("#"+_day+"-"+start_t).attr('rowspan', time_gap);
+
+
+    for(let k=start_t+time_gap;k<=9;k++) {
+        let attr = $("#"+_day+"-"+k).attr('rowspan');
+        // rowspan이 나올때 까지 hide클래스를 지운다.
+        if(typeof attr !== typeof undefined && attr !== false){
+            break;
+        } else {
+            $("#"+_day+"-"+k).removeClass('hide');
+        }
     }
-});
+}
+
+function htmlDownload() {
+    $("#table-download").attr( "href", "https://yeahntto.github.io/ForTimeTable/").get(0).click();
+    $("#table-download").trigger('click');
+
+
+}
